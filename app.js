424de273d4ef5d80768e1e35149b03d57ded624b
Tony Brancato <tonybrancato1@gmail.com>
2017-08-12 12:54:00 -0400

    removed blank lines, adjusted zoom, added you are here pin, added night map

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function success(position) {
     lng: position.coords.longitude
   };
   map.setCenter(pos);
-  map.setZoom(13);
+  map.setZoom(12);
 //Zomato API
   var settings = {
     "async": true,
@@ -27,7 +27,13 @@ function success(position) {
   $.ajax(settings).done(function (response) {
     console.log(response);
     let places = response.nearby_restaurants;
-    let infoWindow; //= new google.maps.InfoWindow
+    let infoWindow;
+    let youAreHere = new google.maps.Marker({
+      position: pos,
+      title: 'You Are Here',
+      map: map,
+      animation: google.maps.Animation.DROP
+    })
     //for each nearby restaurant, create a map marker
     for(i=0; i<places.length; i++) {
       let latLng = new google.maps.LatLng(places[i].restaurant.location.latitude, response.nearby_restaurants[i].restaurant.location.longitude);
@@ -68,13 +74,91 @@ function error() {
   alert("Please enable location services in order to find What's Good in Your Hood!");
 }
 
-
-
 let map, infoWindow;
 function initMap() {
 map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 38.390954, lng: -96.1318693},
-    zoom: 4
+    zoom: 4,
+    styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#38414e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#746855'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{color: '#2f3948'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#17263c'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]
+      }
+    ]
 });
 }
 
@@ -82,7 +166,4 @@ $('#clickMe').click(function () {
   navigator.geolocation.getCurrentPosition(success, error);
 })
 
-
-
 $('.hood-name').hide();
-
